578b1908c3e1fb44930e65cbed6ea2c5758e3a72
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-04 14:55:25 +0530

    Remove descriptions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import { HeaderLayout } from '../layouts/HeaderLayout';
-import { Description, SubThemeDescription } from './views/Description';
 
 export const App = ({ params, children }) =>
 	<HeaderLayout title="CHRI Legal Aid Montoring Tool" logo="http://www.humanrightsinitiative.org/images/logo-orange.png" params={params}>
--- src/components/views/Description.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { topics, sub_themes } from '../../data';
-
-export const Description = ({ topic }) =>
-    <p>{(topics.filter(t => t.url_slug === topic).shift()|| {}).description}</p>;
-
-export const SubThemeDescription = ({ theme, sub_theme }) =>
-    <p className="text-bold">{(sub_themes.filter(t => t.url_slug === sub_theme && t.theme_url_slug === theme).shift()|| {}).description}</p>;
