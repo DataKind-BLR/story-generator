4fd466454db9205a656b084c350d5f7b0b65e27a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-04 12:49:46 +0530

    Update topics with sub theme urls

--- src/data/topics.js
+++ src/data/topics.js
@@ -1,21 +1,21 @@
 export const topics = [
     {
         url_slug: 'legal-counsel-appointed',
-        sub_theme_url_slug: 'access-to-legal-aid/appointments',
+        sub_theme_url_slug: 'first-access-to-legal-aid-police-station-or-court/appointments',
         name: 'Appointment of Legal Counsel',
         description: 'Number of districts where legal aid counsel were appointed for all magistrate courts/ ( Number of districts which responded to the RTI)',
         data: {}
     },
     {
         url_slug: 'panel-lawyers-appointed',
-        sub_theme_url_slug: 'access-to-legal-aid/appointments',
+        sub_theme_url_slug: 'first-access-to-legal-aid-police-station-or-court/appointments',
         name: 'Appointment of Panel Lawyers',
         description: 'Number of districts where panel lawyers were appointed/( Number of districts which responded to the RTI)',
         data: {}
     },
     {
         url_slug: 'retainer-lawyers-appointed',
-        sub_theme_url_slug: 'access-to-legal-aid/appointments',
+        sub_theme_url_slug: 'first-access-to-legal-aid-police-station-or-court/appointments',
         name: 'Appointment of Retainer Lawyers',
         description: 'Number of districts where retainer lawyers were appointed/(Number of districts which responded to the RTI)',
         data: {}
