43f9226d17daedc2ac1ed9689ddcda85f0931c2f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-04 15:09:41 +0530

    Use better topics

--- src/data/themes.js
+++ src/data/themes.js
@@ -1,22 +1,22 @@
 export const themes = [
     {
         'description': 'Access to Legal Aid : Police Station or Court?',
-        'name': 'Access to Legal Aid : Police Station or Court?',
+        'name': 'Early/First Access to Legal Aid',
         'url_slug': 'first-access-to-legal-aid-police-station-or-court'
     },
     {
         'description': 'Legal Aid Clinics: Effective Link?',
-        'name': 'Legal Aid Clinics: Effective Link?',
+        'name': 'Prison Legal Aid Clinics',
         'url_slug': 'prison-legal-aid-clinics-effective-link'
     },
     {
         'description': 'Application to Appointment: Timely Access to Legal Aid?',
-        'name': 'Application to Appointment: Timely Access to Legal Aid?',
+        'name': 'Application to Appointment',
         'url_slug': 'application-to-appointment-timely-access-to-legal-aid'
     },
     {
         'description': 'Aid Watch: Effective Monitoring?',
-        'name': 'Aid Watch: Effective Monitoring?',
+        'name': 'Legal Aid Watch',
         'url_slug': 'legal-aid-watch-effective-monitoring'
     }
 ];
